8f6922d74306fb769eb0166e7c52a966d654be1f
peachanG <kenkman0427@gmail.com>
2022-07-05 13:25:15 +0900

    Add tensorflow & tensorflow-io

--- src/python/lib/lib_a/utils/array.py
+++ src/python/lib/lib_a/utils/array.py
@@ -1,5 +1,11 @@
 import numpy as np
+import tensorflow as tf
+import tensorflow_io as tfio
 
 
 def get_zero_array(shape):
     return np.zeros(shape)
+
+
+def get_zero_tf_array(shape):
+    return tf.zeros(shape)
--- src/python/main/main_a/main.py
+++ src/python/main/main_a/main.py
@@ -1,8 +1,9 @@
 from lib.lib_a.utils.array import get_zero_array
 from lib.lib_a.utils.log import set_logging_basic_config
+from python.lib.lib_a.utils.array import get_zero_tf_array
 
 
 if __name__ == "__main__":
-    get_zero_array([1, 2, 3])
     set_logging_basic_config("INFO")
-
+    print(get_zero_array([1, 2, 3]))
+    print(get_zero_tf_array([1, 2, 3]))
